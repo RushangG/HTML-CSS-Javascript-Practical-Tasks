c9a383342a26f507f8220acca299af05b872f6b9
Rushang Gajera <gajerarushang17@gmail.com>
2026-07-27 12:15:14 +0530

    dashboard dataset use

--- JAVASCRIPT TASK/Extra Task/Data Analysis Dashboard Task/script.js
+++ JAVASCRIPT TASK/Extra Task/Data Analysis Dashboard Task/script.js
@@ -1,3 +1,114 @@
+const productDataSet = [
+    {
+        "id": 1,
+        "title": "Biotique Bio Kelp Protein Shampoo",
+        "category": "beauty",
+        "price": 299.00,
+        "stock": 99
+
+    },
+    {
+        "id": 2,
+        "title": "Lakme Absolute Spotlight Eyeshadow Palette",
+        "category": "beauty",
+        "price": 995.00,
+        "stock": 34
+    },
+    {
+        "id": 3,
+        "title": "Forest Essentials Facial Powder",
+        "category": "beauty",
+        "price": 1450.00,
+        "stock": 89
+    },
+    {
+        "id": 4,
+        "title": "Sugar Cosmetics Matte Attack Lipstick",
+        "category": "beauty",
+        "price": 699.00,
+        "stock": 91
+    },
+    {
+        "id": 5,
+        "title": "Colorbar Vegan Nail Lacquer",
+        "category": "beauty",
+        "price": 225.00,
+        "stock": 79
+    },
+    {
+        "id": 6,
+        "title": "Titan Skinn Raw Eau De Parfum",
+        "category": "fragrances",
+        "price": 2995.00,
+        "stock": 29
+    },
+    {
+        "id": 7,
+        "title": "Bombay Shaving Company Oudh Perfume",
+        "category": "fragrances",
+        "price": 999.00,
+        "stock": 58
+    },
+    {
+        "id": 8,
+        "title": "Kama Ayurveda Pure Rose Water",
+        "category": "fragrances",
+        "price": 1495.00,
+        "stock": 98
+    },
+    {
+        "id": 9,
+        "title": "The Man Company Blanc Body Spray",
+        "category": "fragrances",
+        "price": 349.00,
+        "stock": 4
+    },
+    {
+        "id": 10,
+        "title": "Ajmal Aristocrat Magnolia For Her",
+        "category": "fragrances",
+        "price": 4500.00,
+        "stock": 91
+    },
+    {
+        "id": 11,
+        "title": "Godrej Interio King Size Solid Wood Bed",
+        "category": "furniture",
+        "price": 45000.00,
+        "stock": 88
+    },
+    {
+        "id": 12,
+        "title": "Wakefit 3 Seater Fabric Sofa",
+        "category": "furniture",
+        "price": 18500.00,
+        "stock": 60
+    },
+    {
+        "id": 13,
+        "title": "The Sleep Company Bedside Table Sheesham Wood",
+        "category": "furniture",
+        "price": 4999.00,
+        "stock": 64
+    },
+    {
+        "id": 14,
+        "title": "Green Soul Ergonomic Office Chair",
+        "category": "furniture",
+        "price": 8999.00,
+        "stock": 26
+    },
+    {
+        "id": 15,
+        "title": "Pepperfry Engineered Wood Washbasin Cabinet",
+        "category": "furniture",
+        "price": 12499.00,
+        "stock": 7
+    }
+];
+
+
+
 
 
 // Global Variables
@@ -6,11 +117,12 @@ let products = [];
 // Chart Variables
 let barChart;
 let lineChart;
+let pieChart;
 
 
 // add categories to select dropdown
 function loadCategories() {
- 
+
     const select = document.getElementById('category-select');
     const categories = [];
 
@@ -68,6 +180,7 @@ function renderTable(data) {
             '<td>' + data[i].category + '</td>' +
             '<td>' + data[i].price + ' Rs</td>' +
             '<td>' + data[i].stock + '</td>' +
+            '<td>' + (data[i].price * data[i].stock).toFixed(2) + ' Rs</td>' +
             '</tr>';
         body.innerHTML += row;
     }
@@ -79,11 +192,13 @@ function renderCharts(data) {
     const titles = [];
     const prices = [];
     const stocks = [];
+    const totalAmount = [];
 
     for (let i = 0; i < data.length; i++) {
         titles.push(data[i].title);
         prices.push(data[i].price);
         stocks.push(data[i].stock);
+        totalAmount.push(data[i].price * data[i].stock);
     }
 
     // if charts alrdedy exist so destroy them.
@@ -93,6 +208,10 @@ function renderCharts(data) {
 
     if (lineChart) {
         lineChart.destroy();
+    }
+
+    if (pieChart) {
+        pieChart.destroy();
     }
 
 
@@ -107,6 +226,22 @@ function renderCharts(data) {
                 label: 'Stock',
                 data: stocks
             }]
+        },
+        options: {
+            plugins: {
+                legend: { display: false },
+                title: {
+                    display: true,
+                    text: 'Stock of Products',
+                    font: {
+                        size: 20,
+                        color: 'black',
+                        family: 'Segoe UI'
+                    },
+                    fontColor: 'black'
+                }
+            },
+
         }
     });
 
@@ -122,7 +257,40 @@ function renderCharts(data) {
                 data: prices,
                 borderColor: 'green'
             }]
+        },
+        options: {
+            plugins: {
+                legend: { display: false },
+                title: {
+                    display: true,
+                    text: 'Price of Products',
+                    font: {
+                        size: 20,
+                        color: 'black',
+                        family: 'Segoe UI'
+                    }
+
+                }
+            }
         }
+    });
+
+    const chart3 = document.getElementById('myPieChart');
+    pieChart = new Chart(chart3, {
+        type: 'pie',
+        data: {
+            labels: titles,
+            datasets: [{
+                label: 'Total(Price * Stock)',
+                data: totalAmount
+            }]
+        },
+        options: {
+            plugins: {
+                legend: { display: false },
+            }
+        }
+
     });
 
 }
@@ -151,15 +319,17 @@ function filterData() {
     }
 }
 
-// Fetch Data from API
-async function apiFetch() {
+// Fetch Data 
+async function FetchData() {
 
-    const res = await fetch('https://dummyjson.com/products?limit=15');
+    // const res = await fetch('https://dummyjson.com/products?limit=15');
 
-    const data = await res.json();
+    // const data = await res.json();
 
-    console.log(data);
-    products = data.products;
+
+    console.log(productDataSet);
+    // set products to productDataSet
+    products = productDataSet;
     loadCategories();
     displayDashboard(products);
 }
@@ -168,4 +338,4 @@ async function apiFetch() {
 document.getElementById('category-select').addEventListener('change', filterData);
 
 //dom load.
-document.addEventListener('DOMContentLoaded', apiFetch);
+document.addEventListener('DOMContentLoaded', FetchData);
